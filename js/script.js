e97379dc0650c5db01c18c44531117c55569a56c
Stefano-Bernini <stefanobernini26@gmail.com>
2023-06-19 16:16:56 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ createApp({
     mounted(){
         for(let i=1; i<=10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
-                this.arrayEmail.push(response.data.response)
+                this.arrayEmail.push(response.data.response);
             })
         }
     }
